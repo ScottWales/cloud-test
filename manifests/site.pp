590dc6ab818114c12a58706c17317d7dfdb080d1
Scott Wales <scott.wales@unimelb.edu.au>
2012-10-23 09:47:39 +1100

    Import not working

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,16 +1,4 @@
-# A stupid class that just creates a test file in /tmp, so that
-# we can see if the puppet infrastructure is working or not.
-
-class helloworld {
-	file { '/tmp/helloworld':
-		ensure => present,
-		content => "Hello World!",
-	}
-}
-
-import "classes/*"
-
 node default {
-    include helloworld
     include python-2.7
+    include clang
 }
