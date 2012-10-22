c924773b6fa7896ef51d68aca44763743b1939ae
Scott Wales <scott.wales@unimelb.edu.au>
2012-10-23 10:52:11 +1100

    Add git repos

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,5 +1,30 @@
 import "classes/*"
+
+class git {
+    package {'git':
+        ensure => latest,
+    }
+}
+define git::repo ($repo) {
+    include git
+    exec { "git clone $source $title":  
+        creates => $title
+    }
+}
+#define pip::package {
+#    include pip
+#    exec { "pip install $title":
+#    }
+#}
+
+class test-repo {
+    git::repo{'/tmp/repo':
+        source => "git://github.com/ScottWales/cloud-test"
+    }
+}
+
 node default {
     include 'python-2_7'
     include 'clang'
+    include test-repo
 }
